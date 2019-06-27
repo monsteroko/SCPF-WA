07ceaa38da3990b3eaf587fe5c682a6dfd24630e
o.oliinyk <o.oliinyk@student.csn.khai.edu>
2019-06-27 13:15:21 +0300

    Menu ReDesign

--- Assets/Settings.cs
+++ Assets/Settings.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
     public bool isFullScreen=true;
     public AudioMixer am;
+    Resolution[] rsl;
+    List<string> resolutions;
+    public Dropdown dropdown;
+
+    public void Awake()
+    {
+        resolutions = new List<string>();
+        rsl = Screen.resolutions;
+        foreach (var i in rsl)
+        {
+            resolutions.Add(i.width + "x" + i.height);
+        }
+        dropdown.ClearOptions();
+        dropdown.AddOptions(resolutions);
+    }
 
     public void FullScreenToggle()
     {
@@ -18,5 +34,15 @@ public class Settings : MonoBehaviour
     public void AudioVolume(float sliderValue)
     {
         am.SetFloat("masterVolume", sliderValue);
+    }
+
+    public void Quality(int q)
+    {
+        QualitySettings.SetQualityLevel(q);
+    }
+
+    public void Resolution(int r)
+    {
+        Screen.SetResolution(rsl[r].width, rsl[r].height, isFullScreen);
     }
 }
