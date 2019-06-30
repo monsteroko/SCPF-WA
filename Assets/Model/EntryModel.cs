bd7d4afb5b4e3fbd923c3176d82950b660ac8a7a
Rezz <valerarezz@gmail.com>
2019-07-01 00:23:20 +0300

    Создать модель SCP объекта

---
+++ Assets/Model/EntryModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class EntryModel {
+    string name;
+    string code;
+    int type;
+    string description;
+    string procedures;
+    bool isStatic;
+
+    public static EntryModel LoadFromFile(string filename) {
+        var text = File.ReadAllText(EntryManager.AppendedSubPath(filename));
+        return JsonConvert.DeserializeObject<EntryModel>(text);
+    }
+}
---
+++ Assets/Model/GameModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModel : MonoBehaviour
+{
+
+    EntryManager entityManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        entityManager = new EntryManager();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
