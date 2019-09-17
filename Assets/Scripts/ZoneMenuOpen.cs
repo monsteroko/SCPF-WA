c518015958f72ed435a49abe9e38cccad6af3000
Slowpokew <yabatrak@gmail.com>
2019-09-18 02:07:18 +0300

    Выезжающая и заезжающая панель по нажатию на кубик на карте, и еще что-то по мелочи забыл что

---
+++ Assets/Scripts/ZoneMenuOpen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ZoneMenuOpen : MonoBehaviour
+{
+    public GameObject ZoneManagement;
+    int Current = 0, Limit = 0, v = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Current < Limit && Limit == 500)
+        {
+            ZoneManagement.transform.Translate(v * -1, 0, 0);
+            Current += v;
+        }
+
+        if (Current > Limit && Limit == 0)
+        {
+            ZoneManagement.transform.Translate(v * 1, 0, 0);
+            Current -= v;
+        }
+
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Gavno!");
+        if (Current == 0)
+        {
+            Limit = 500;
+        }
+
+        if (Current == 500)
+        {
+            Limit = 0;
+        }
+        
+        
+    }
+}
