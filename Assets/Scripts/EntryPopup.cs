d20080b59cd94e076a8b3fecb04ab0a81304be47
Oleg Oleynik <oko241999@gmail.com>
2019-09-18 14:29:07 +0300

    Сделал обьект SCP на карте

--- Assets/Scripts/EntryPopup.cs
+++ Assets/Scripts/EntryPopup.cs
@@ -6,12 +6,14 @@ using UnityEngine.Events;
 
 public class EntryPopup : MonoBehaviour {
 
-    public float timer;
     public Text nameText;
     public Text classText;
     public Text descriptionText;
     public Button okButton;
     public Button cancButton;
+    public float timer;
+    public GameObject Cube;
+    public int x, y;
 
     public GameObject entryPopupObject;
 
@@ -21,7 +23,6 @@ public class EntryPopup : MonoBehaviour {
         entryPopup = FindObjectOfType(typeof(EntryPopup)) as EntryPopup;
         entryPopupObject.SetActive(false);
     }
-
     public static EntryPopup Instance() {
         return entryPopup;
     }
@@ -30,12 +31,13 @@ public class EntryPopup : MonoBehaviour {
         nameText.text = "SCP-" + entry.code + ": " + entry.name;
         classText.text = "Класс: " + entry.type;
         descriptionText.text = "Описание: " + entry.description;
+        okButton.onClick.AddListener(ClosePopup);
         okButton.onClick.RemoveAllListeners();
         okButton.onClick.AddListener(ClosePopup);
         cancButton.onClick.RemoveAllListeners();
         cancButton.onClick.AddListener(ClosePopup);
         entryPopupObject.SetActive(true);
-        timer = 0;
+        
         Time.timeScale = timer;
     }
 
@@ -43,5 +45,11 @@ public class EntryPopup : MonoBehaviour {
         timer = 1f;
         Time.timeScale = timer;
         entryPopupObject.SetActive(false);
+    }
+    public void OnButtonDown()
+    {
+        x = Random.Range(0, 200);
+        y = Random.Range(-100, 0);
+        Cube.transform.Translate(x, y,-2);
     }
 }
--- Assets/Scripts/ShowEntryPopup.cs
+++ Assets/Scripts/ShowEntryPopup.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ShowEntryPopup : MonoBehaviour
 {
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = 0f;
+        Time.timeScale = timer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Time.timeScale = timer;
     }
 }
