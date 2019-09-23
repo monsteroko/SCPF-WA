871134ecf8c25d6c24c3c1d270b7ae73cea7e9b7
meandzoloft <valerarezz@gmail.com>
2019-09-23 19:40:52 +0300

    Create map manager

---
+++ Assets/Model/MapSaveModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSaveModel
+{
+    public readonly List<string> unlockedRegions;
+    
+    private MapSaveModel(List<string> unlockedRegions)
+    {
+        this.unlockedRegions = unlockedRegions;
+    }
+
+    public static MapSaveModel NewGameSave(string counterpartname) 
+    {
+        List<string> unlockedRegions = new List<string>();
+        switch (counterpartname)
+        {
+            case "eeu":
+                unlockedRegions.Add("Kharkiv");
+                break;
+            case "na":
+                unlockedRegions.Add("Iowa");
+                break;
+            case "sa":
+                unlockedRegions.Add("Buenos Aires");
+                break;
+            default:
+                Debug.Log("Wrong starting counterpart");
+                break;
+        }
+        return new MapSaveModel(unlockedRegions);
+    }
+}
