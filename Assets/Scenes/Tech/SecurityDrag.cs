bb6cc0309bf7ddf6656bcafb0d364ec53bae9ce7
Slowpokew <yabatrak@gmail.com>
2019-10-09 01:11:30 +0300

    цифры пытались меня наебать

---
+++ Assets/Scenes/Tech/SecurityDrag.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class SecurityDrag : MonoBehaviour
+{
+    public GameObject panel;
+    
+    void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(/*Input.mousePosition.x*/0, Input.mousePosition.y, 0));
+        panel.transform.position += newPosition;
+    }    
+}
--- Assets/Scenes/Tech/UITech.cs
+++ Assets/Scenes/Tech/UITech.cs
@@ -19,34 +19,42 @@ public class UITech : MonoBehaviour
     private int lim, now = 0;
     private Vector3 vector;
 
-    // Start is called before the first frame update
+    private int scrHeight;
+    private int scrWidth;
+
+
     void Start()
     {
         sec.onClick.AddListener(SecurityTech);
         con.onClick.AddListener(ContainmentTech);
         pro.onClick.AddListener(ProtectionTech);
-        secB.onClick.AddListener(SecurityTech);
-        conB.onClick.AddListener(ContainmentTech);
-        proB.onClick.AddListener(ProtectionTech);
+        secB.onClick.AddListener(SecurityTechB);
+        conB.onClick.AddListener(ContainmentTechB);
+        proB.onClick.AddListener(ProtectionTechB);
         back.onClick.AddListener(Back);
+        now = 0;
+        scrWidth = Screen.currentResolution.width;
+        scrHeight = Screen.currentResolution.height;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (lim > now && lim == 70)
+        if (lim > now && lim >= 70)
         {
             Move();
             now++;
             now++;
         }
 
-        if (lim < now && lim == 0)
+        if (lim < now && lim <= 0)
         {
             Move();
             now--;
             now--;
         }
+
+        //panel.transform.position = Vector3.up * 271 + Vector3.right * 482;
     }
 
     private void Move()
@@ -74,23 +82,21 @@ public class UITech : MonoBehaviour
 
     public void SecurityTechB()
     {
-        Debug.Log(lim);
-        Debug.Log(now);
-        Debug.Log(vector);
+        panel.transform.position = Vector3.down * 290 + Vector3.right * 482;
         vector = Vector3.up;
         lim = 0;
-        Debug.Log(lim);
-        Debug.Log(vector);
     }
 
     public void ContainmentTechB()
     {
+        panel.transform.position = Vector3.up * 757 + Vector3.right * (1462);
         vector = Vector3.down + Vector3.left * 2;
         lim = 0;
     }
 
     public void ProtectionTechB()
     {
+        panel.transform.position = Vector3.up * 831 + Vector3.left * (640);
         vector = Vector3.down + Vector3.right * 2;
         lim = 0;
     }
