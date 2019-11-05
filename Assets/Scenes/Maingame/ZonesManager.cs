5088ecb45b71cd51c51dba167053a9110c28a19c
Oleg Oleynik <oko241999@gmail.com>
2019-11-06 00:01:28 +0200

    Fix to Zone Managment window

--- Assets/Scenes/Maingame/ZonesManager.cs
+++ Assets/Scenes/Maingame/ZonesManager.cs
@@ -25,6 +25,8 @@ public class ZonesManager : MonoBehaviour
         zoneObject.transform.position = map.transform.TransformPoint(city.unity2DLocation);
         zoneObject.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
         zoneObject.transform.rotation = Quaternion.Euler(90.0f, 0, 0);
+        BoxCollider boxCollider = zoneObject.AddComponent<BoxCollider>();
+        boxCollider.size= new Vector3(8.0f, 2.0f, 9.0f);
         return zone;
     }
 }
